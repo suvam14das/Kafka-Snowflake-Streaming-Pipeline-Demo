b6387acf3739bf557ab5f7cd4b57e72d8812ff3d
suvam14das <suvam14das@gmail.com>
2026-04-26 16:46:23 +0530

    weather api streaming init

---
+++ weather-api-producer.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime, timezone
+import requests
+import json
+from kafka import KafkaProducer
+
+API_KEY = "6b12030e61695e0e2352bb6755e088f9"
+CITY = "Bangalore"
+BASIC_URL_TEMPLATE = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+
+print(BASIC_URL_TEMPLATE)
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092','localhost:9093','localhost:9094'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+    acks='all',   # important for no data loss
+    retries=5
+)
+
+def fetch_weather():
+    
+    return requests.get(BASIC_URL_TEMPLATE).json()
+
+while True:
+    data = fetch_weather()
+    data["main"]["dt"] = str(datetime.now(timezone.utc))
+    producer.send("weather-data", value=data)
+    producer.flush()
+    print("Sent:", data)
+    time.sleep(10)
---
+++ weather-stream-consumer.py
@@ -0,0 +1,37 @@
+from kafka import KafkaConsumer
+import snowflake.connector
+import json
+
+consumer = KafkaConsumer(
+    'weather-data',
+    bootstrap_servers=['localhost:9092','localhost:9093','localhost:9094'],
+    group_id='weather-consumer-group',
+    auto_offset_reset='earliest',
+    enable_auto_commit=False,  # critical
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+conn = snowflake.connector.connect(
+    user='suvam14das',
+    password='Snowflakecloud@01',
+    account='KVOCKGI-SZ77270'
+)
+
+cursor = conn.cursor()
+
+cursor.execute(""" USE DATABASE WEATHER """)
+
+for message in consumer:
+    data = message.value
+    print("Message conusmed : ",data)
+    
+    # simple insert (better: stage + merge)
+    cursor.execute("""
+        INSERT INTO WEATHER_TABLE (DATA)
+        SELECT PARSE_JSON(%s)
+    """, (json.dumps(data),))
+    
+    conn.commit()
+    
+    # commit Kafka offset ONLY after Snowflake success
+    consumer.commit()
